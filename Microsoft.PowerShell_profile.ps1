ce17553be22b2dd8e950925e957f6230f268c8f3
crisleo94 <cristhian.reinoso@live.com>
2025-11-08 21:46:21 -0500

    merge conflicts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -37,19 +37,6 @@ function prompt {
   "> "
 }
 
-# git alias
-#to create new aliases that might require arguments in any context follow the same pattern
-function Get-Git { & git $args }
-New-Alias -Name g -Value Get-Git -Force -Option AllScope
-function Get-Git-Status { & git status $args }
-New-Alias -Name gs -Value Get-Git-Status -Force -Option AllScope
-function Get-Git-Log { & git log $args }
-New-Alias -Name gl -Value Get-Git-Log -Force -Option AllScope
-function Get-Git-Add { & git add $args }
-New-Alias -Name gad -Value Get-Git-Add -Force -Option AllScope
-function Get-Git-Commit { & git commit $args }
-New-Alias -Name gcm -Value Get-Git-Commit -Force -Option AllScope
-
 # python alias
 function Get-Python { & python $args }
 New-Alias -Name py -Value Get-Python -Force -Option AllScope
@@ -61,5 +48,51 @@ function Get-Docker { & docker $args }
 New-Alias -Name d -Value Get-Docker -Force -Option AllScope
 function Get-Docker-Compose { & docker-compose $args }
 New-Alias -Name dc -Value Get-Docker-Compose -Force -Option AllScope
+
+# git alias
+function Get-Git { & git $args }
+New-Alias -Name g -Value Get-Git -Force -Option AllScope
+# git status alias
+function Get-GitStatus { & git status $args }
+New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
+# git commit alias
+function Get-GitCommit { & git commit -m $args }
+New-Alias -Name gcm -Value Get-GitCommit -Force -Option AllScope
+# git add alias
+function Get-GitAdd { & git add $args }
+New-Alias -Name gad -Value Get-GitAdd -Force -Option AllScope
+# git gitree alias
+function Get-GitTree { & git log --graph --oneline --decorate $args }
+New-Alias -Name gtr -Value Get-GitTree -Force -Option AllScope
+# git push alias
+function Get-GitPush { & git push origin $args }
+New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+# git pull alias
+function Get-GitPull { & git pull origin $args }
+New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
+# git fetch alias
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
+# git checkout alias
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope
+# git checkout branch alias
+function Get-GitCheckoutBranch { & git checkout -b $args }
+New-Alias -Name gcob -Value Get-GitCheckoutBranch -Force -Option AllScope
+# git branch alias
+function Get-GitBranch { & git branch $args }
+New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
+# git remote alias
+function Get-GitRemote { & git remote $args }
+New-Alias -Name gre -Value Get-GitRemote -Force -Option AllScope
+# git clone
+function Get-GitClone { & git clone $args }
+New-Alias -Name gcl -Value Get-GitClone -Force -Option AllScope
+# git stash
+function Get-GitStash { & git stash $args }
+New-Alias -Name gst -Value Get-GitStash -Force -Option AllScope
+# pipenv
+function Get-Pipenv { & pipenv $args }
+New-Alias -Name pv -Value Get-Pipenv -Force -Option AllScope
 
 Invoke-Expression (&starship init powershell)
